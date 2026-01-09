4c9f82bc265dc6aa0d5d80b168613b6673aa4f08
d3f4l7d <idaten.bekko@gmail.com>
2026-01-09 10:25:26 +0900

    Update zshautoinstall.sh

--- zshautoinstall.sh
+++ zshautoinstall.sh
@@ -1,1 +1,13 @@
+echo 
+echo "    _   _   _ _____ ___ ___ _   _ ____ _____  _    _     _     "
+echo "   / \ | | | |_   _/ _ \_ _| \ | / ___|_   _|/ \  | |   | |    "
+echo "  / _ \| | | | | || | | | ||  \| \___ \ | | / _ \ | |   | |    "
+echo " / ___ \ |_| | | || |_| | || |\  |___) || |/ ___ \| |___| |___ "
+echo "/_/   \_\___/  |_| \___/___|_| \_|____/ |_/_/   \_\_____|_____|"
+echo
+
+sudo pacman -Syu
 sudo pacman -S kitty zsh ttf-firacode wget
+sudo chsh -s /bin/zsh d3f4l7d
+sudo cat /etc/passwd | grep d3f4l7d
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
