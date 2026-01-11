26ed4565b40fc0fcf092e25c3f3e4320c1425ac5
d3f4l7d <idaten.bekko@gmail.com>
2026-01-11 20:09:16 +0900

    Update nvidiainstall.sh

--- nvidiainstall.sh
+++ nvidiainstall.sh
@@ -6,3 +6,7 @@ echo "| |\  | \ V /  | || |_| | | / ___ \ | || |\  |___) || |/ ___ \| |___| |___
 echo "|_| \_|  \_/  |___|____/___/_/   \_\___|_| \_|____/ |_/_/   \_\_____|_____|"
 echo
 
+echo "This script is tested && worked w/ Nvidia GeForce RTX4090 (AD102) on MSI motherboard (bootloader: systemd)"
+
+sudo pacman -Syu --noconfirm
+
