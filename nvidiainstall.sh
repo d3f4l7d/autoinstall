b68a66a2dc96744fcf40b5d187bb47593f6a2ce4
d3f4l7d <idaten.bekko@gmail.com>
2026-01-11 21:00:28 +0900

    Update nvidiainstall.sh

--- nvidiainstall.sh
+++ nvidiainstall.sh
@@ -10,3 +10,21 @@ echo "This script is tested && worked w/ Nvidia GeForce RTX4090 (AD102) on MSI m
 
 sudo pacman -Syu --noconfirm
 
+sudo pacman -S linux-firmware-nvidia nvidia-open-dkms nvidia-settings nvidia-utils opencl-nvidia nvtop nvhpc
+mkdir ~/.config/gcc12
+mkdir ~/.config/cuda
+mkdir ~/.config/cudnn
+wget -O ~/.config/gcc12/gcc12-12.3.0-6-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/g/gcc12/gcc12-12.3.0-6-x86_64.pkg.tar.zst
+wget -O ~/.config/gcc12/gcc12-libs-12.3.0-6-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/g/gcc12-libs/gcc12-libs-12.3.0-6-x86_64.pkg.tar.zst
+wget -O ~/.config/cuda/cuda-12.3.2-1-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/c/cuda/cuda-12.3.2-1-x86_64.pkg.tar.zst
+wget -O ~/.config/cudnn/cudnn-8.9.7.29-1-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/c/cudnn/cudnn-8.9.7.29-1-x86_64.pkg.tar.zst
+cd ~/.config/gcc12
+sudo pacman -U gcc12-12.3.0-6-x86_64.pkg.tar.zst  gcc12-libs-12.3.0-6-x86_64.pkg.tar.zst
+cd ~/.config/cuda
+sudo pacman -U cuda-12.3.2-1-x86_64.pkg.tar.zst
+cd ~/.config/cudnn
+sudo pacman -U cudnn-8.9.7.29-1-x86_64.pkg.tar.zst
+cd
+echo 'export PATH="/opt/cuda/bin:$PATH"' >> ~/.zshrc
+echo 'export CUDA_HOME="/opt/cuda"' >> ~/.zshrc
+
