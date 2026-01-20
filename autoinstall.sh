0f5c5f40c68a586f8ed5320bf142431018ec31fd
d3f4l7d <idaten.bekko@gmail.com>
2026-01-20 11:13:37 +0900

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -124,27 +124,28 @@ if [ ! -f ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage ]; then
     mkdir -p ~/.local/share/AppImage
     wget -O ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage https://github.com/arduino/arduino-ide/releases/download/2.3.7/arduino-ide_2.3.7_Linux_64bit.AppImage 
     chmod +x ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage
-    mkdir -p ~/.local/share/application
-    mkdir -p ~/.local/share/icon
+    echo "only applications & icons are acceptable"
+    mkdir -p ~/.local/share/applications
+    mkdir -p ~/.local/share/icons
     cp -r ~/.config/d3f4l7d/autoinstall/icon ~/.local/share
     sudo usermod -a -G uucp d3f4l7
 
     echo "Create App Interface (ArduinoProIDE) in ~/.local/share/application/arduino-ide_2.3.7_Linux_64bit.desktop"
     echo "[Desktop Entry]"
-    echo "Name=ArduinoProIDE"
-    echo "Exec=~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage %u"
-    echo "Icon=~/.local/share/icon/ArduinoCommunityLogo-up.png"
+    echo "Name=Arduino2xx"
+    echo "Exec=/home/d3f4l7/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage %u"
+    echo "Icon=/home/d3f4l7/.local/share/icons/ArduinoCommunityLogo-up.png"
     echo "Type=Application"
     echo "Categories=Development;"
-    echo "StartupNotify=false"
+    echo "StartupNotify=true"
     echo "Terminal=false"
-    echo "Keywords=clang;ide;"
-    echo "update-desktop-database ~/.local/share/application"
+    echo "Keywords=Editor;"
+    echo "update-desktop-database /home/d3f4l7/.local/share/application"
 
     echo "Create /etc/xdg/autostart/inod.desktop for autostart."
     echo "[Desktop Entry]"
     echo "Name=inod"
-    echo "Exec=~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage"
+    echo "Exec=/home/d3f4l7/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage"
     echo "Type=Application"
     echo "Terminal=false"
     echo "autoinstall of Arduino -- DONE"
