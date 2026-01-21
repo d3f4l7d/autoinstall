5205ad59655f2acf72d808247ee206f8f1a3343e
d3f4l7d <idaten.bekko@gmail.com>
2026-01-21 14:34:41 +0900

    Update softinstall.sh

--- softinstall.sh
+++ softinstall.sh
@@ -8,6 +8,75 @@ echo
 
 sudo pacman -Syu --noconfirm
 
+if [ ! -f ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage ]; then
+    echo "Arduino AppImage (ver. 2.3.7) not found!"
+    echo
+    echo " ___ _   _  ___  "
+    echo "|_ _| \ | |/ _ \ "
+    echo " | ||  \| | | | |"
+    echo " | || |\  | |_| |"
+    echo "|___|_| \_|\___/ "
+    echo
+
+    sudo pacman -S --noconfirm fuse
+    mkdir -p ~/.local/share/AppImage
+    wget -O ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage https://github.com/arduino/arduino-ide/releases/download/2.3.7/arduino-ide_2.3.7_Linux_64bit.AppImage 
+    chmod +x ~/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage
+    echo "only applications & icons are acceptable"
+    mkdir -p ~/.local/share/applications
+    mkdir -p ~/.local/share/icons
+    cp -r ~/.config/d3f4l7d/autoinstall/icon ~/.local/share
+    sudo usermod -a -G uucp d3f4l7
+
+    echo "Create App Interface (ArduinoProIDE) in ~/.local/share/application/arduino-ide_2.3.7_Linux_64bit.desktop"
+    echo "[Desktop Entry]"
+    echo "Name=Arduino2xx"
+    echo "Exec=/home/d3f4l7/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage %u"
+    echo "Icon=/home/d3f4l7/.local/share/icons/ArduinoCommunityLogo-up.png"
+    echo "Type=Application"
+    echo "Categories=Development;"
+    echo "StartupNotify=true"
+    echo "Terminal=false"
+    echo "Keywords=Editor;"
+    echo "update-desktop-database /home/d3f4l7/.local/share/application"
+
+    echo "Create /etc/xdg/autostart/inod.desktop for autostart."
+    echo "[Desktop Entry]"
+    echo "Name=inod"
+    echo "Exec=/home/d3f4l7/.local/share/AppImage/arduino-ide_2.3.7_Linux_64bit.AppImage"
+    echo "Type=Application"
+    echo "Terminal=false"
+    echo "autoinstall of Arduino -- DONE"
+else
+    echo "Arduino is already installed. Skip INO process..."
+fi
+
+if pacman -Q ibus &> /dev/null; then
+    echo "ibus is already installed. Skip IBUS process..."
+else
+    echo
+    echo " ___ ____  _   _ ____  "
+    echo "|_ _| __ )| | | / ___| "
+    echo " | ||  _ \| | | \___ \ "
+    echo " | || |_) | |_| |___) |"
+    echo "|___|____/ \___/|____/ "
+    echo
+
+    sudo pacman -Sy --noconfirm ibus ibus-anthy noto-fonts-cjk
+    echo 'export GTK_IM_MODULE=ibus' >> ~/.zshrc
+    echo 'export GT_IM_MODULE=ibus' >> ~/.zshrc
+    echo 'export XMODIFIERS=@im=ibus' >> ~/.zshrc
+
+    echo "Create /etc/xdg/autostart/ibusd.desktop for autostart."
+    echo "[Desktop Entry]"
+    echo "Name=ibusd"
+    echo "Exec=ibus-daemon -rxRd"
+    echo "Type=Application"
+    echo "Terminal=false"
+    echo "Encoding=UTF-8"
+    echo "autoinstall of ibus -- DONE"
+fi
+
 if pacman -Q bluez &> /dev/null; then
     echo "bluez is already installed. Skip BLUE process..."
 else
@@ -25,6 +94,24 @@ else
 
     echo "Uncomment #AutoEnable=true in /etc/bluetooth/main.conf"
     echo "autoinstall of bluetooth -- DONE"
+fi
+
+if pacman -Q cups &> /dev/null; then
+    echo "cups is already installed. Skip CUPS process..."
+else
+    echo
+    echo "   ____ _   _ ____  ____  "
+    echo "  / ___| | | |  _ \/ ___| "
+    echo " | |   | | | | |_) \___ \ "
+    echo " | |___| |_| |  __/ ___) |"
+    echo "  \____|\___/|_|   |____/ "
+    echo
+
+    sudo pacman -S --noconfirm cups libxml2-legacy
+    sudo systemctl start cups
+    sudo systemctl enable cups
+
+    echo "autoinstall of cups -- DONE"
 fi
 
 if pacman -Q gvfs-mtp &> /dev/null; then
